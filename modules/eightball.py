8a135c049137041862233509fe41893a966e2695
Misingnoglic <arya@aryaboudaie.com>
2013-04-07 17:18:59 -0700

    Made code more politically correct
    Maybe I shouldn't have swear words in my code huh

--- modules/eightball.py
+++ modules/eightball.py
@@ -8,12 +8,12 @@ eightball.commands = ['8']
 
 def addresponse(phenny, input):
     foo = open("8ball.txt")
-    shit= (input.group()).split()
+    stuff= (input.group()).split()
     phrases = str(foo.read())#.split("^~`")
-    if "^~`" in str( " ".join(shit[1:]) ):
-        phenny.say("Go fuck yourself")
+    if "^~`" in str( " ".join(stuff[1:]) ):
+        phenny.say("Invalid Characters")
     else:
-        newphrases = phrases+"^~`"+str( " ".join(shit[1:]) )
+        newphrases = phrases+"^~`"+str( " ".join(stuff[1:]) )
         foo = open("8ball.txt", 'w')
         foo.write(newphrases)
         phenny.say(str(newphrases.split("^~`")))
@@ -22,8 +22,8 @@ addresponse.commands = ['8add']
 
 def removeresponse(phenny, input):
     foo = open("8ball.txt")
-    shit= ((input.group()).split())
-    shat = str(" ".join(shit[1:]))
+    stuff= ((input.group()).split())
+    shat = str(" ".join(stuff[1:]))
     phrases = (foo.read()).split("^~`")
     if shat not in phrases:
         phenny.say("Not Found to delete")
@@ -47,3 +47,7 @@ def showresponses(phenny,input):
     phenny.say(str(phrases))
 showresponses.commands = ["8show"]
 
+def ballhelp(phenny,input):
+    phenny.say ("Type .8 to get your answer, .8add ANSWER to add a result, .8del ANSWER to delete an existing answer, .8show to show all possible answers, and .8default to reset the answers. Have fun!")
+
+ballhelp.commands = ['8help'] 
