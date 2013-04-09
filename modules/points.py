c670bbd634b83a01e26087746274e7e758aacc37
Misingnoglic <arya@aryaboudaie.com>
2013-04-09 00:44:55 -0700

    Several changes to points
    Names can be more than one word, not case sensitive
    added move command variations
    added score resetting option
    scores can all be said even if too long

--- modules/points.py
+++ modules/points.py
@@ -1,13 +1,14 @@
 def showpoints(phenny, input):
     foo = eval((open('points.txt')).read())
-    name=((input.group()).split())[1]
+    name=(' '.join(((input.group()).split())[1:])).lower()
     if name in foo.keys(): phenny.say(name + ': '+str(foo[name]))
     else:
         foo[name] = 0
         new = open('points.txt', 'w')
         new.write(str(foo))
+        phenny.say("User was previously not in database, now added with 0 points")
 
-showpoints.commands = ["showpoints"]
+showpoints.commands = ["showpoints", "showscore", 'score']
 
 def addpoints(phenny, input):
     foo = eval((open('points.txt')).read())
@@ -22,7 +23,7 @@ def addpoints(phenny, input):
         new = open('points.txt', 'w')
         new.write(str(foo))
         phenny.say(name+": "+str(foo[name]))
-addpoints.commands = ['addpoint']
+addpoints.commands = ['addpoint', 'addpoints', 'pluspoint']
 
 def subpoint(phenny, input):
     foo = eval((open('points.txt')).read())
@@ -37,7 +38,7 @@ def subpoint(phenny, input):
         new = open('points.txt', 'w')
         new.write(str(foo))
         phenny.say(name+": "+str(foo[name]))
-subpoint.commands = ['subpoint', 'delpoint']
+subpoint.commands = ['subpoint', 'delpoint', 'subpoints', 'delpoints']
 
 
 def winner(phenny, input):
@@ -50,7 +51,7 @@ def winner(phenny, input):
             if foo[x]==high: winners +=[x]
         phenny.say(', '.join(winners)+ " with "+str(foo[winners[0]])+" points")
 
-winner.commands = ["winners"]
+winner.commands = ["winners",'winner']
 
 def scores(phenny, input):
     scores = ''
@@ -62,13 +63,41 @@ def scores(phenny, input):
             for x in foo.keys():
                 if foo[x]==high:
                     scores += (x+": "+str(foo[x])+' ')
-                    del foo[x]
+                    del foo[x]        
+        for string in breakupstring(scores):
+            phenny.say(string)
 
-        phenny.say(scores)
+        
 
-scores.commands = ['scores']
+
+scores.commands = ['scores','leaders','showscores']
+
+def scoresraw(phenny, input):
+    foo = eval((open('points.txt')).read())
+    phenny.say(str(foo))
+
+scoresraw.commands = ['scoresraw']
+def resetscore(phenny, input):
+    foo = eval((open('points.txt')).read())
+    name=(' '.join(((input.group()).split())[1:])).lower()
+    if name in foo:
+        del foo[name]
+        new = open('points.txt', 'w')
+        new.write(str(foo))
+        phenny.say('score reset for user')
+
+resetscore.commands = ['resetscore','resetpoints']
+
+def breakupstring(string):
+    LOS = []
+    while len(string)>437:
+        newstr = string[:437]
+        LOS+=[newstr]
+        string = string[437:]
+    LOS += [string]
+    return LOS
 
 def pointshelp(phenny, input):
-    phenny.say('''Welcome to points, where the points don't matter! Type .showpoints USER to see their points, .addpoint USER to give them a point, .subpoint USER to make them lose a point, .winners to see who is in the lead, and .scores to see all the scores. Have fun!''' )
+    phenny.say('''Welcome to points, where the points don't matter! Type .showpoints USER to see their points, .addpoint USER to give them a point, .subpoint USER to make them lose a point, .resetscore USER to reset someone's score, .winners to see who is in the lead, and .scores to see all the scores. Have fun!''' )
 
-pointshelp.commands = ['pointshelp']
+pointshelp.commands = ['pointshelp','phelp']
