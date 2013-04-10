416adf10ee56ec464af50985fed593fcf62689c9
Mehrzad Far <mehrzadfar@gmail.com>
2013-04-09 17:56:36 -0700

    added duckluck

--- modules/duck.py
+++ modules/duck.py
@@ -15,3 +15,11 @@ def lmddg(phenny, input):
 
 lmddg.commands = ['lmddg','lmddgtfy','lmd','lm']
 
+def duckluck(phenny, input):
+    phrase = input.group(2)
+    phrase = phrase.split()
+    phrase = '%20'.join(phrase)
+    phenny.say('https://duckduckgo.com/?q=!%20'+phrase)
+
+duckluck.commands = ['dl','duckluck','imfeelingducky']
+
