1e903c7d559de3cbe1f377d5fbf16a927042ec53
Misingnoglic <arya@aryaboudaie.com>
2013-04-11 23:55:16 -0700

    added respond
    Made a module for giving the bot a code and it returns a response. Can
    add responses too.

---
+++ modules/respond.py
@@ -0,0 +1,27 @@
+def returnresponse(phenny,input):
+    foo = eval((open('responses.txt')).read())
+    key = str(input.group(2))
+    try:
+        phenny.say(str(foo[key]))
+    except:
+        phenny.say("Item not found")
+
+returnresponse.commands = ['r','respond']
+
+def addresponse(phenny,input):
+    foo = eval((open('responses.txt')).read())
+    inp = str(input.group(2)).split()
+    if inp[0] in foo.keys():
+        phenny.say("Keyword already being used!")
+    else:
+        foo[inp[0]]= str(inp[1])
+        new = open('points.txt', 'w')
+        new.write(str(foo))
+        phenny.say("Response Added!")
+
+addresponse.commands = ['addr','addresponse']
+
+def responsehelp(phenny,input):
+    phenny.say("To get a response type .r CODE. To add one type .r CODE RESPONSE. I intended this to save links but you can use it for anything")
+    
+responsehelp.commands = ['rhelp']
